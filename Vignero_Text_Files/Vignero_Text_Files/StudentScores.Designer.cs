3f1e62eba4d7325833aa474cb572df8da16a079c
David Vignero <viggy815@aol.com>
2017-08-07 17:37:18 -0400

    upload
    uploaded entire project file with all changes

---
+++ Vignero_Text_Files/Vignero_Text_Files/StudentScores.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Vignero_Text_Files
+{
+    partial class StudentScores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.gbStudentScores = new System.Windows.Forms.GroupBox();
+            this.lblScores = new System.Windows.Forms.Label();
+            this.gbHighestScore = new System.Windows.Forms.GroupBox();
+            this.lblHighest = new System.Windows.Forms.Label();
+            this.gbAverage = new System.Windows.Forms.GroupBox();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.gbStudentScores.SuspendLayout();
+            this.gbHighestScore.SuspendLayout();
+            this.gbAverage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 130);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 159);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 188);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // gbStudentScores
+            // 
+            this.gbStudentScores.Controls.Add(this.lblScores);
+            this.gbStudentScores.Location = new System.Drawing.Point(103, 12);
+            this.gbStudentScores.Name = "gbStudentScores";
+            this.gbStudentScores.Size = new System.Drawing.Size(180, 319);
+            this.gbStudentScores.TabIndex = 3;
+            this.gbStudentScores.TabStop = false;
+            this.gbStudentScores.Text = "Student Scores";
+            // 
+            // lblScores
+            // 
+            this.lblScores.AutoSize = true;
+            this.lblScores.Location = new System.Drawing.Point(6, 28);
+            this.lblScores.Name = "lblScores";
+            this.lblScores.Size = new System.Drawing.Size(0, 13);
+            this.lblScores.TabIndex = 0;
+            // 
+            // gbHighestScore
+            // 
+            this.gbHighestScore.Controls.Add(this.lblHighest);
+            this.gbHighestScore.Location = new System.Drawing.Point(289, 12);
+            this.gbHighestScore.Name = "gbHighestScore";
+            this.gbHighestScore.Size = new System.Drawing.Size(200, 100);
+            this.gbHighestScore.TabIndex = 4;
+            this.gbHighestScore.TabStop = false;
+            this.gbHighestScore.Text = "Student w/Highest Score";
+            // 
+            // lblHighest
+            // 
+            this.lblHighest.AutoSize = true;
+            this.lblHighest.Location = new System.Drawing.Point(34, 46);
+            this.lblHighest.Name = "lblHighest";
+            this.lblHighest.Size = new System.Drawing.Size(0, 13);
+            this.lblHighest.TabIndex = 0;
+            // 
+            // gbAverage
+            // 
+            this.gbAverage.Controls.Add(this.lblAverage);
+            this.gbAverage.Location = new System.Drawing.Point(289, 231);
+            this.gbAverage.Name = "gbAverage";
+            this.gbAverage.Size = new System.Drawing.Size(200, 100);
+            this.gbAverage.TabIndex = 5;
+            this.gbAverage.TabStop = false;
+            this.gbAverage.Text = "Average Score";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(82, 48);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(0, 13);
+            this.lblAverage.TabIndex = 0;
+            // 
+            // StudentScores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 374);
+            this.Controls.Add(this.gbAverage);
+            this.Controls.Add(this.gbHighestScore);
+            this.Controls.Add(this.gbStudentScores);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStart);
+            this.Name = "StudentScores";
+            this.Text = "Student Scores";
+            this.gbStudentScores.ResumeLayout(false);
+            this.gbStudentScores.PerformLayout();
+            this.gbHighestScore.ResumeLayout(false);
+            this.gbHighestScore.PerformLayout();
+            this.gbAverage.ResumeLayout(false);
+            this.gbAverage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox gbStudentScores;
+        private System.Windows.Forms.GroupBox gbHighestScore;
+        private System.Windows.Forms.GroupBox gbAverage;
+        private System.Windows.Forms.Label lblScores;
+        private System.Windows.Forms.Label lblHighest;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
+
---
+++ Vignero_Text_Files/Vignero_Text_Files/StudentScores.cs
@@ -0,0 +1,139 @@
+﻿/*Student Scores- Text Files
+ * David Vignero
+ * POS/409
+ * John Becton
+ * March 13, 2017
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Vignero_Text_Files
+{
+    public partial class StudentScores : Form
+
+    {
+        //declaring objects and variables
+        ArrayList scoreList = new ArrayList();
+        ArrayList score = new ArrayList();
+        double average;
+        double sum;
+    
+        int counter;
+        public StudentScores()
+        {
+            InitializeComponent();
+            //creating text file when the applicaiton is loaded and adding values
+            StreamWriter writer = new StreamWriter("StudentScores.txt", false);
+            writer.WriteLine("David Jones, 78");
+            writer.WriteLine("Josh Smith, 73");
+            writer.WriteLine("Mike Wells, 68");
+            writer.WriteLine("Kevin Brokovich, 91");
+            writer.WriteLine("Stephen Brooks, 71");
+            writer.WriteLine("Marshall Stewart, 90");
+            writer.WriteLine("John Parks, 63");
+            writer.WriteLine("Jackie Walker, 95");
+            writer.WriteLine("Nicole Roberts, 79");
+            writer.WriteLine("Paul Stout, 57");
+            writer.Close();
+        }
+        //code used to exit the application
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //code used to clear the fields and put that to their default value
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblAverage.ResetText();
+            lblHighest.ResetText();
+            lblScores.ResetText();
+
+        }
+        //code to handle the start button
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string line = null;
+            string dir = Path.GetFullPath("StudentScores.txt");
+            try
+            {
+                lblScores.ResetText();
+                
+           
+                StreamReader reader = new StreamReader(dir);
+                // loops through the lines in the text file
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //adds each line to an arraylist
+                    scoreList.Add(line);
+                    //splits the values of each line to add the numeric value to its own arraylist
+                    string[] pieces = line.Split();
+                    score.Add(pieces[2]);
+               //loops through the values of the score arraylist
+                  for(int x = 0; x < score.Count; x++)
+                    {
+                        //code to sort the arraylist in order to get the highet score
+                        score.Sort();
+                        score.Reverse();
+                        if (score[0] == pieces[2])
+                        {
+                            //outputs the highest score and the student that achieved the highest score
+                            lblHighest.Text = string.Format(pieces[0] + " " + pieces[1] + " " + pieces[2] + "%\n");
+                        }
+
+
+                    }
+                   
+
+
+                }
+             
+              //loops through arraylist and ouputs all the scores and student names
+                for(int x = 0; x <= scoreList.Count -1; x++)
+                {
+                    lblScores.Text += scoreList[x].ToString()+"%\n\n";
+               
+                }
+                //closing reader object
+                reader.Close();
+               //calling method
+                getAverage();
+            }
+            catch (Exception j)
+            {
+                MessageBox.Show(j.ToString());
+            }
+        }
+        //method used to get the average score
+        public void getAverage()
+
+        {
+
+            counter = 0;
+            sum = 0;
+            average = 0;
+            //loops through until counter is 1 less then the length of the score arraylist
+             while (counter < score.Count)
+             {
+                //constantly calculates the average while it loops through to get the final average when it loops through the final time
+                 sum = sum + double.Parse(score[counter].ToString());
+              average = sum / score.Count;
+                lblAverage.Text = average.ToString()+"%";
+                counter++;
+            }
+          //clearing both arraylist to avoid duplicate data when the applicaiton is cleared and started, or if the start button it selected before clearing
+            score.Clear();
+            scoreList.Clear();
+        }
+    }
+}
